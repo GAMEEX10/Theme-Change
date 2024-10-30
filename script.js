c7b2db526b8746314bfbf14f7c5a27cf8078291e
shakir <shakirex10@gmail.com>
2024-10-30 08:33:03 -0700

    creating color educational game idea for kids

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var index= 0;
 
 function x(){
-    var g = ["red","green","blue","yellow","white","black","orange","purple"];
-    document.getElementsByTagName("body")[0].style.background=g[index++];
+    var g = ["red","green","blue","yellow","black","orange","purple"];
+    document.getElementsByTagName("div")[0].style.background=g[index++];
     if(index>g.length-1)
     index=0;
 }
